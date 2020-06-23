6b317e39f0e359547579bb716af5f94717a42d0f
Luc Urlings <urlingsluc@gmail.com>
2020-06-23 14:32:58 +0200

    feature/KL-103: added like and unlike functionality

---
+++ kwet-service/Models/LikeModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace kwet_service.Models
+{
+    public class LikeModel
+    {
+        [Required] public string Username { get; set; }
+        [Required] public Guid UserId { get; set; }
+    }
+}
